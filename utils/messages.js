87f4b0fbbdd5aac510980e89f1b166b5ef40266f
Anwara Favour <favourchika02@gmail.com>
2023-08-05 21:17:21 +0100

    update

--- utils/messages.js
+++ utils/messages.js
@@ -5,9 +5,11 @@ function formatMessage(username, text) {
     return {
        username,
        text,
-       time: moment().format('h:mm a')
+    time: moment().format('h:mm a')
     }
 }
 
 
 module.exports = formatMessage;
+
+console.log('Hello World')
