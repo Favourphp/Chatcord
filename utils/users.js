2284214edc2ec4786539f7aafe8e3952c40fbfc1
Anwara Favour <favourchika02@gmail.com>
2023-08-10 22:42:26 +0100

    updated

--- utils/users.js
+++ utils/users.js
@@ -10,15 +10,15 @@ function userJoin(id, username, room) {
 }
 
 //get current user
-function getCurrentUser(id) {
-    return users.find(user => user.id === id);
-}
+// function getCurrentUser(id) {
+//     return users.find(user => user.id === id);
+//}
 
 //user leaves chat
 function userLeave(id) {
 const index = users.findIndex(user => user.id === id);
   if(index !== -1) {
-    return users.splice(index, 1)[0];
+   // return users.splice(index, 1)[0];
   }
 }
 //get room users
